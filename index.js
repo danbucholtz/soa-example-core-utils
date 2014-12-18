5bbff81aa9b0ab28df94a0b369edf63d12ea3da9
Dan Bucholtz <dan.bucholtz@gmail.com>
2014-12-18 10:29:53 -0600

    testing...

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-var createBaseUrl = function(protocol, host, port){
-	return protocol + "://" + host + ":" + port;
+var config = require("soa-example-service-config").config();
+
+var crypto = require('crypto');
+
+var createBaseUrl = function(host, port){
+	return config.protocol + "://" + host + ":" + port;
+};
+
+var hashPassword = function(password, salt){
+	return crypto.createHash("sha256").update(password).update(salt).digest("hex");
 };
 
 module.exports = {
