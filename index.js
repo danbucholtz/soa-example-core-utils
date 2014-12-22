0b50b8250d61bb1454bff950a11be7e760ef790c
Dan Bucholtz <dan.bucholtz@gmail.com>
2014-12-22 13:33:32 -0600

    testing...

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var config = require("soa-example-service-config").config();
 var crypto = require('crypto');
+var Q = require("q");
 
 var algorithm = 'aes256'; // or any other algorithm supported by OpenSSL
 var key = 't0k3|\|$br0';
@@ -22,11 +23,112 @@ var decryptString = function(input){
 	var decipher = crypto.createDecipher(algorithm, key);
 	var output = decipher.update(input, 'hex', 'utf8') + decipher.final('utf8');
 	return output;
-}
+};
+
+var get = function(url){
+	getInternal(false, null, null, null, url);
+};
+
+var getWithAccessToken = function(accessToken, url){
+	getInternal(true, null, null, accessToken, url);
+};
+
+var getWithEmailAddressAndPassword = function(emailAddress, password, url){
+	getInternal(true, emailAddress, password, null, url);
+};
+
+var getInternal = function(authEnabled, emailAddress, password, accessToken, url){
+	var deferred = Q.defer();
+
+	if ( authEnabled ){
+		request.get(url, function(err, response, body){
+			if ( err ){
+				deferred.reject(err);
+			}
+			else{
+				deferred.resolve(body);
+			}
+		}).auth(emailAddress, password, accessToken);
+	}
+	else{
+		request.get(url, function(err, response, body){
+			if ( err ){
+				deferred.reject(err);
+			}
+			else{
+				deferred.resolve(body);
+			}
+		});
+	}
+
+	return deferred.promise;
+};
+
+var postJson = function(objectToPost, url){
+	return postJsonInternal(false, null, null, null, objectToPost, url);
+};
+
+var postJsonWithAccessToken = function(accessToken, objectToPost, url){
+	return postJsonInternal(true, null, null, accessToken, objectToPost, url);
+};
+
+var postJsonWithEmailAddressAndPassword = function(emailAddress, password, objectToPost, url){
+	return postJsonInternal(true, emailAddress, password, null, objectToPost, url);
+};
+
+var postJsonInternal = function(authEnabled, emailAddress, password, accessToken, objectToPost, url){
+	var deferred = Q.defer();
+
+	var jsonToPost = objectToJson(objectToPost);
+
+	var options = {
+		uri: url,
+		method: "POST",
+		json: jsonToPost
+	};
+
+	if ( authEnabled ){
+		request(options, function(error, response, body){
+
+			if ( error ){
+				deferred.reject(error.toString());
+				return;
+			}
+
+			deferred.resolve(body);
+
+		}).auth(emailAddress, password, accessToken);
+	}
+	else{
+		request(options, function(error, response, body){
+
+			if ( error ){
+				deferred.reject(error.toString());
+				return;
+			}
+
+			deferred.resolve(body);
+
+		});
+	}
+	
+
+	return deferred;
+};
+
+var objectToJson = function(object){
+	return JSON.stringify(object);
+};
 
 module.exports = {
 	createBaseUrl : createBaseUrl,
 	hashPassword: hashPassword,
 	encryptString: encryptString,
-	decryptString: decryptString
+	decryptString: decryptString,
+	postJson: postJson,
+	postJsonWithAccessToken: postJsonWithAccessToken,
+	postJsonWithEmailAddressAndPassword: postJsonWithEmailAddressAndPassword,
+	get: get,
+	getWithAccessToken: getWithAccessToken,
+	getWithEmailAddressAndPassword: getWithEmailAddressAndPassword
 }
